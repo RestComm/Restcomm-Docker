61724b0afc8036b5a1d20f435fecda23b62b42fe
lefty <eleftherios.banos@telestax.com>
2016-01-19 16:12:19 +0200

    fix certificate issue for self-signed .

--- scripts/restcomm_sslconf.sh
+++ scripts/restcomm_sslconf.sh
@@ -68,7 +68,8 @@ if [ -n "$SECURESSL" ]; then
     keytool -importcert -alias startssl -keystore $TRUSTSTORE_FILE -storepass $TRUSTSTORE_PASSWORD -file $BASEDIR/ca-authority.der -noprompt
   elif [ "$SECURESSL" = "SELF"  ]; then
     echo "TRUSTSTORE_FILE is not provided but SECURE is TRUE. We will create and configure self signed certificate"
-    HOSTNAME=`echo $RESTCOMMHOST`
+    #HOSTNAME=`echo $RESTCOMMHOST`
+    HOSTNAME=`hostname`
     echo $HOSTNAME
     TRUSTSTORE_LOCATION=$TRUSTSTORE_FILE
     keytool -genkey -alias $TRUSTSTORE_ALIAS -keyalg RSA -keystore $TRUSTSTORE_LOCATION -dname "CN=$HOSTNAME" -storepass $TRUSTSTORE_PASSWORD -keypass $TRUSTSTORE_PASSWORD
