4432382d26888de0437682c6c39d54326e1afc1f
lefty <eleftherios.banos@telestax.com>
2016-01-22 13:46:23 +0200

    Fixes for issues #18 & #17

--- scripts/restcomm_sslconf.sh
+++ scripts/restcomm_sslconf.sh
@@ -59,51 +59,53 @@ fi
 
 if [ -n "$SECURESSL" ]; then
   echo "SECURE $SECURESSL"
-  # Add StartComm certificate to the truststore to avoid SSL Exception when fetching the URL
-  keytool -importcert -alias startssl -keystore /usr/lib/jvm/java-7-oracle/jre/lib/security/cacerts -storepass changeit -file $BASEDIR/ca-startcom.der -noprompt
+  if [[ "$SECURESSL" = "SELF" ||  "$SECURESSL" = "AUTH" ]]; then
+      # Add StartComm certificate to the truststore to avoid SSL Exception when fetching the URL
+      keytool -importcert -alias startssl -keystore /usr/lib/jvm/java-7-oracle/jre/lib/security/cacerts -storepass changeit -file $BASEDIR/ca-startcom.der -noprompt
 
-  sed -i "s/DISABLE_HTTP=.*/DISABLE_HTTP='true'/" $BASEDIR/bin/restcomm/restcomm.conf
-  sed -i "s/TRUSTSTORE_FILE=.*/TRUSTSTORE_FILE='$TRUSTSTORE_FILE_NAME'/" $BASEDIR/bin/restcomm/restcomm.conf
-  #Certificate setup (Authority certificate or self-signed)
-  if [ "$SECURESSL" = "AUTH" ]; then
-    keytool -importcert -alias $TRUSTSTORE_ALIAS -keystore $TRUSTSTORE_FILE -storepass $TRUSTSTORE_PASSWORD -file $BASEDIR/ca-authority.der -noprompt
-  elif [ "$SECURESSL" = "SELF"  ]; then
-    echo "TRUSTSTORE_FILE is not provided but SECURE is TRUE. We will create and configure self signed certificate"
+      sed -i "s/DISABLE_HTTP=.*/DISABLE_HTTP='true'/" $BASEDIR/bin/restcomm/restcomm.conf
+      sed -i "s/TRUSTSTORE_FILE=.*/TRUSTSTORE_FILE='$TRUSTSTORE_FILE_NAME'/" $BASEDIR/bin/restcomm/restcomm.conf
+      #Certificate setup (Authority certificate or self-signed)
+      if [ "$SECURESSL" = "AUTH" ]; then
+        keytool -importcert -alias $TRUSTSTORE_ALIAS -keystore $TRUSTSTORE_FILE -storepass $TRUSTSTORE_PASSWORD -file $BASEDIR/ca-authority.der -noprompt
+      elif [ "$SECURESSL" = "SELF"  ]; then
+        echo "TRUSTSTORE_FILE is not provided but SECURE is TRUE. We will create and configure self signed certificate"
 
-     TRUSTSTORE_LOCATION=$TRUSTSTORE_FILE
-    if [ -n "$RESTCOMMHOST" ]; then
-        HOSTNAME=`echo $RESTCOMMHOST`
-        keytool -genkey -alias $TRUSTSTORE_ALIAS -keyalg RSA -keystore $TRUSTSTORE_LOCATION -dname "CN=$HOSTNAME" -storepass $TRUSTSTORE_PASSWORD -keypass $TRUSTSTORE_PASSWORD
-    else
-        HOSTNAME=`echo $STATIC_ADDRESS`
-        keytool -genkey -alias $TRUSTSTORE_ALIAS -keyalg RSA -keystore $TRUSTSTORE_LOCATION -dname "CN=restcomm" -ext san=ip:"$HOSTNAME" -storepass $TRUSTSTORE_PASSWORD -keypass $TRUSTSTORE_PASSWORD
-    fi
-    echo $HOSTNAME
+         TRUSTSTORE_LOCATION=$TRUSTSTORE_FILE
+        if [ -n "$RESTCOMMHOST" ]; then
+            HOSTNAME=`echo $RESTCOMMHOST`
+            keytool -genkey -alias $TRUSTSTORE_ALIAS -keyalg RSA -keystore $TRUSTSTORE_LOCATION -dname "CN=$HOSTNAME" -storepass $TRUSTSTORE_PASSWORD -keypass $TRUSTSTORE_PASSWORD
+        else
+            HOSTNAME=`echo $STATIC_ADDRESS`
+            keytool -genkey -alias $TRUSTSTORE_ALIAS -keyalg RSA -keystore $TRUSTSTORE_LOCATION -dname "CN=restcomm" -ext san=ip:"$HOSTNAME" -storepass $TRUSTSTORE_PASSWORD -keypass $TRUSTSTORE_PASSWORD
+        fi
+        echo $HOSTNAME
 
-    echo "The generated truststore file at $TRUSTSTORE_LOCATION "
-  fi
+        echo "The generated truststore file at $TRUSTSTORE_LOCATION "
+      fi
 
-  sed -i "s|protocol=\"TLSv1,TLSv1.1,TLSv1.2\"|protocol=\"TLSv1,TLSv1.1,TLSv1.2,SSLv2Hello\"|" $BASEDIR/standalone/configuration/standalone-sip.xml
-  grep -q 'ephemeralDHKeySize' $BASEDIR/bin/standalone.conf || sed -i "s|-Djava.awt.headless=true|& -Djdk.tls.ephemeralDHKeySize=2048|" $BASEDIR/bin/standalone.conf
-  grep -q 'allowLegacyHelloMessages' $BASEDIR/bin/standalone.conf || sed -i "s|-Djava.awt.headless=true|& -Dsun.security.ssl.allowLegacyHelloMessages=false -Djsse.enableSNIExtension=false|" $BASEDIR/bin/standalone.conf
-  grep -q 'https.protocols' $BASEDIR/bin/standalone.conf || sed -i "s|-Djava.awt.headless=true|& -Dhttps.protocols=TLSv1.1,TLSv1.2|" $BASEDIR/bin/standalone.conf
+      sed -i "s|protocol=\"TLSv1,TLSv1.1,TLSv1.2\"|protocol=\"TLSv1,TLSv1.1,TLSv1.2,SSLv2Hello\"|" $BASEDIR/standalone/configuration/standalone-sip.xml
+      grep -q 'ephemeralDHKeySize' $BASEDIR/bin/standalone.conf || sed -i "s|-Djava.awt.headless=true|& -Djdk.tls.ephemeralDHKeySize=2048|" $BASEDIR/bin/standalone.conf
+      grep -q 'allowLegacyHelloMessages' $BASEDIR/bin/standalone.conf || sed -i "s|-Djava.awt.headless=true|& -Dsun.security.ssl.allowLegacyHelloMessages=false -Djsse.enableSNIExtension=false|" $BASEDIR/bin/standalone.conf
+      grep -q 'https.protocols' $BASEDIR/bin/standalone.conf || sed -i "s|-Djava.awt.headless=true|& -Dhttps.protocols=TLSv1.1,TLSv1.2|" $BASEDIR/bin/standalone.conf
 
-  grep -q 'connector name="sip-wss"' $BASEDIR/standalone/configuration/standalone-sip.xml || sed -i '/connector name=\"sip-ws\".*/ a \
-	\ <connector name="sip-wss" protocol="SIP/2.0" scheme="sip" socket-binding="sip-wss"/>
-	' $BASEDIR/standalone/configuration/standalone-sip.xml
-  if [ -n "$STATIC_ADDRESS" ]; then
-	sed -i "s|<connector name=\"sip-wss\" .*/>|<connector name=\"sip-wss\" protocol=\"SIP/2.0\" scheme=\"sip\" socket-binding=\"sip-wss\" use-static-address=\"true\" static-server-address=\"`echo $STATIC_ADDRESS`\" static-server-port=\"5083\"/>|" $BASEDIR/standalone/configuration/standalone-sip.xml
-  fi
-  grep -q 'binding name="sip-wss"' $BASEDIR/standalone/configuration/standalone-sip.xml || sed -i '/binding name=\"sip-ws\".*/ a \
-	\<socket-binding name="sip-wss" port="5083"/>' $BASEDIR/standalone/configuration/standalone-sip.xml
-   grep -q 'gov.nist.javax.sip.TLS_CLIENT_AUTH_TYPE=Disabled' $BASEDIR/standalone/configuration/mss-sip-stack.properties ||
-   sed -i '/org.mobicents.ha.javax.sip.LOCAL_SSL_PORT=443/ a \
-  \gov.nist.javax.sip.TLS_CLIENT_AUTH_TYPE=Disabled\
-  \javax.net.ssl.keyStore='"$TRUSTSTORE_FILE"'\
-  \javax.net.ssl.keyStorePassword='"`echo $TRUSTSTORE_PASSWORD`"'\
-  \javax.net.ssl.trustStorePassword='"`echo $TRUSTSTORE_PASSWORD`"'\
-  \javax.net.ssl.trustStore='"$TRUSTSTORE_FILE"'\
-  \javax.net.ssl.keyStoreType=JKS' $BASEDIR/standalone/configuration/mss-sip-stack.properties
+      grep -q 'connector name="sip-wss"' $BASEDIR/standalone/configuration/standalone-sip.xml || sed -i '/connector name=\"sip-ws\".*/ a \
+        \ <connector name="sip-wss" protocol="SIP/2.0" scheme="sip" socket-binding="sip-wss"/>
+        ' $BASEDIR/standalone/configuration/standalone-sip.xml
+      if [ -n "$STATIC_ADDRESS" ]; then
+        sed -i "s|<connector name=\"sip-wss\" .*/>|<connector name=\"sip-wss\" protocol=\"SIP/2.0\" scheme=\"sip\" socket-binding=\"sip-wss\" use-static-address=\"true\" static-server-address=\"`echo $STATIC_ADDRESS`\" static-server-port=\"5083\"/>|" $BASEDIR/standalone/configuration/standalone-sip.xml
+      fi
+      grep -q 'binding name="sip-wss"' $BASEDIR/standalone/configuration/standalone-sip.xml || sed -i '/binding name=\"sip-ws\".*/ a \
+        \<socket-binding name="sip-wss" port="5083"/>' $BASEDIR/standalone/configuration/standalone-sip.xml
+       grep -q 'gov.nist.javax.sip.TLS_CLIENT_AUTH_TYPE=Disabled' $BASEDIR/standalone/configuration/mss-sip-stack.properties ||
+       sed -i '/org.mobicents.ha.javax.sip.LOCAL_SSL_PORT=443/ a \
+      \gov.nist.javax.sip.TLS_CLIENT_AUTH_TYPE=Disabled\
+      \javax.net.ssl.keyStore='"$TRUSTSTORE_FILE"'\
+      \javax.net.ssl.keyStorePassword='"`echo $TRUSTSTORE_PASSWORD`"'\
+      \javax.net.ssl.trustStorePassword='"`echo $TRUSTSTORE_PASSWORD`"'\
+      \javax.net.ssl.trustStore='"$TRUSTSTORE_FILE"'\
+      \javax.net.ssl.keyStoreType=JKS' $BASEDIR/standalone/configuration/mss-sip-stack.properties
+   fi
 fi
 
 
