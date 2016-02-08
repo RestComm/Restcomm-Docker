7650d655b45baf55c0f9b4879a869fdf817ff264
lefty <eleftherios.banos@telestax.com>
2016-02-08 18:46:13 +0200

    Load Balancer files permission error.

--- scripts/restcomm_toolsconf.sh
+++ scripts/restcomm_toolsconf.sh
@@ -37,6 +37,9 @@ cp -rf $BASEDIR/mms-server-beans.xml $BASEDIR/mediaserver/deploy/server-beans.xm
 sed -i 's/configLogDirectory$/#configLogDirectory/' $BASEDIR/dont-config-mobicents-ms.sh
 cp -rf $BASEDIR/dont-config-mobicents-ms.sh $BASEDIR/bin/restcomm/autoconfig.d/dont-config-mobicents-ms.sh
 
+#Load balancer
+chmod 777 $BASEDIR/tools/sip-balancer/lb-log4j.xml
+chmod 777 $BASEDIR/tools/sip-balancer/lb-configuration.properties
 
 chmod +x $BASEDIR/bin/*.sh
 chmod +x $BASEDIR/bin/restcomm/*.sh
