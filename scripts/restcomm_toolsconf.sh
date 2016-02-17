f89a6ff485df32464cecc1bab747073d5e2faeb4
lefteris <liblefty@gmail.com>
2016-02-17 14:27:30 +0200

    Possibility to disable  the standard ports usage.

--- scripts/restcomm_toolsconf.sh
+++ scripts/restcomm_toolsconf.sh
@@ -15,7 +15,7 @@ if [ -n "$SECURESSL" ]; then
   sed -i "s|5082|5083|" $BASEDIR/standalone/deployments/olympus.war/resources/js/controllers/register.js
 fi
 
-if [ -n "$USE_STANDARD_PORTS" ]; then
+if [  "$USE_STANDARD_PORTS" = "TRUE"  ]; then
   sed -i "s|5082|5062|" $BASEDIR/standalone/deployments/olympus.war/resources/js/controllers/register.js
   sed -i "s|5083|5063|" $BASEDIR/standalone/deployments/olympus.war/resources/js/controllers/register.js
 fi
