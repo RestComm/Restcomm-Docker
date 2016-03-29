0756616f392031ffb44821ad8516cdf7c49d7d3f
lefty <eleftherios.banos@telestax.com>
2016-03-29 18:20:47 +0300

    Fix cp removing echo.

--- scripts/restcomm_toolsconf.sh
+++ scripts/restcomm_toolsconf.sh
@@ -46,9 +46,9 @@ chmod 777 $BASEDIR/tools/sip-balancer/lb-configuration.properties
 chmod +x $BASEDIR/bin/*.sh
 chmod +x $BASEDIR/bin/restcomm/*.sh
 chmod +x /opt/embed/*.sh
-mkdir -p "$(RESTCOMM_LOGS)"/opt/
-cp /tmp/version "$(RESTCOMM_LOGS)"
-cp /opt/embed/dockercleanup.sh  "$(RESTCOMM_LOGS)"/opt/
-cp /opt/embed/restcomm_docker.sh  "$(RESTCOMM_LOGS)"/opt/
+mkdir -p "${RESTCOMM_LOGS}"/opt/
+cp /tmp/version "${RESTCOMM_LOGS}"
+cp /opt/embed/dockercleanup.sh  "${RESTCOMM_LOGS}"/opt/
+cp /opt/embed/restcomm_docker.sh  "${RESTCOMM_LOGS}"/opt/
 
 echo "RestComm configured Properly!"
