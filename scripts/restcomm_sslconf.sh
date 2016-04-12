bcfbab5575f56269692ca6bf47cb6573d4f3b54a
lefty <eleftherios.banos@telestax.com>
2016-04-12 17:18:27 +0300

    Remove offset for wss, is done from jboss.

--- scripts/restcomm_sslconf.sh
+++ scripts/restcomm_sslconf.sh
@@ -129,14 +129,14 @@ if [  "${USE_STANDARD_SIP_PORTS^^}" = "TRUE"  ] ; then
       sed -i "s|5083|5063|" $BASEDIR/standalone/configuration/standalone-sip.xml
 fi
 
-if [ -n "$PORT_OFFSET" ]; then
-    if [  "${USE_STANDARD_SIP_PORTS^^}" = "TRUE"  ]; then
-        wss=$((5063 + $PORT_OFFSET))
-         sed -i "s|5063|${wss}|" $BASEDIR/bin/restcomm/autoconfig.d/config-sip-connectors.sh
-         sed -i "s|5063|${wss}|" $BASEDIR/standalone/configuration/standalone-sip.xml
-    else
-        wss=$((5083 + $PORT_OFFSET))
-         sed -i "s|5083|${wss}|" $BASEDIR/bin/restcomm/autoconfig.d/config-sip-connectors.sh
-         sed -i "s|5083|${wss}|" $BASEDIR/standalone/configuration/standalone-sip.xml
-    fi
-fi
+#if [ -n "$PORT_OFFSET" ]; then
+#    if [  "${USE_STANDARD_SIP_PORTS^^}" = "TRUE"  ]; then
+#        wss=$((5063 + $PORT_OFFSET))
+#         sed -i "s|5063|${wss}|" $BASEDIR/bin/restcomm/autoconfig.d/config-sip-connectors.sh
+#         sed -i "s|5063|${wss}|" $BASEDIR/standalone/configuration/standalone-sip.xml
+#    else
+#        wss=$((5083 + $PORT_OFFSET))
+#         sed -i "s|5083|${wss}|" $BASEDIR/bin/restcomm/autoconfig.d/config-sip-connectors.sh
+#         sed -i "s|5083|${wss}|" $BASEDIR/standalone/configuration/standalone-sip.xml
+#    fi
+#fi
