df20c5470c3b3701b7b76a1373a6e6d70b23d566
lefty <eleftherios.banos@telestax.com>
2016-05-06 13:36:10 +0300

    Fix for start container.

--- scripts/restcomm_conf.sh
+++ scripts/restcomm_conf.sh
@@ -411,7 +411,7 @@ if [ -n "$RESTCOMM_TRACE_LOG" ]; then
 
     TCPFILE="/etc/my_init.d/restcommtrace.sh"
     cat <<EOT >> $TCPFILE
-    #!/bin/bash
+#!/bin/bash
     nohup xargs bash -c "tcpdump -pni any -t -n -s 0  \"portrange 5060-5063 or (udp and portrange 65000-65535) or port 80 or port 443 or port 2427 or port 2727\" -G 3500 -w $LOGS_TRACE/$RESTCOMM_TRACE_LOG/restcomm_trace_%Y-%m-%d_%H:%M:%S-%Z.pcap -z gzip" &
 EOT
     chmod 777 $TCPFILE
