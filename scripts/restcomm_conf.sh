559c5a4a304878399e99ab638f8967bfe7a6611a
lefty <eleftherios.banos@telestax.com>
2016-05-25 14:02:10 +0300

    AAdd TCP control for HA.

--- scripts/restcomm_conf.sh
+++ scripts/restcomm_conf.sh
@@ -307,6 +307,7 @@ if [  "${USE_STANDARD_SIP_PORTS^^}" = "TRUE"  ]; then
   sed -i "s|SIP_PORT_WSS=.*|SIP_PORT_WSS=5063|"   $BASEDIR/bin/restcomm/restcomm.conf
 fi
 
+
 if [ -n "$PORT_OFFSET" ]; then
     echo "PORT_OFFSET $PORT_OFFSET"
     sed -i "s|PORT_OFFSET=.*|SIP_PORT_UDP=${PORT_OFFSET}|"   $BASEDIR/bin/restcomm/restcomm.conf
@@ -391,12 +392,27 @@ if [ -n "$RESTCOMM_TRACE_LOG" ]; then
   mkdir -p $LOGS_TRACE/$RESTCOMM_TRACE_LOG
   sed -i "s|find .*restcomm_trace_|find $LOGS_TRACE/`echo $RESTCOMM_TRACE_LOG`/restcomm_trace_|" /etc/cron.d/restcommtcpdump-cron
   sed -i "s|RESTCOMM_TRACE=.*|RESTCOMM_TRACE=\$RESTCOMM_LOG_BASE/`echo $RESTCOMM_TRACE_LOG`|"  /opt/embed/restcomm_docker.sh
-  nohup xargs bash -c "tcpdump -pni any -t -n -s 0  \"portrange 5060-5063 or (udp and portrange 65000-65535) or port 80 or port 443 or port 2427 or port 2727\" -G 3500 -w $LOGS_TRACE/$RESTCOMM_TRACE_LOG/restcomm_trace_%Y-%m-%d_%H:%M:%S-%Z.pcap -z gzip" &
 
-  TCPFILE="/etc/my_init.d/restcommtrace.sh"
-    cat <<EOT >> $TCPFILE
-#!/bin/bash
+   ps cax | grep tcpdump > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "TCPDUMP  is running."
+  else
+    echo "TCPDUMP is not running, need to run it."
     nohup xargs bash -c "tcpdump -pni any -t -n -s 0  \"portrange 5060-5063 or (udp and portrange 65000-65535) or port 80 or port 443 or port 2427 or port 2727\" -G 3500 -w $LOGS_TRACE/$RESTCOMM_TRACE_LOG/restcomm_trace_%Y-%m-%d_%H:%M:%S-%Z.pcap -z gzip" &
+  fi
+
+ #Used to start TCPDUMP when restarting container
+ TCPFILE="/etc/my_init.d/restcommtrace.sh"
+    cat <<EOT >> $TCPFILE
+    #!/bin/bash
+
+     ps cax | grep tcpdump > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "TCPDUMP  is running."
+  else
+    echo "TCPDUMP is not running, need to run it."
+    nohup xargs bash -c "tcpdump -pni any -t -n -s 0  \"portrange 5060-5063 or (udp and portrange 65000-65535) or port 80 or port 443 or port 2427 or port 2727\" -G 3500 -w $LOGS_TRACE/$RESTCOMM_TRACE_LOG/restcomm_trace_%Y-%m-%d_%H:%M:%S-%Z.pcap -z gzip" &
+  fi
 EOT
     chmod 777 $TCPFILE
 fi
