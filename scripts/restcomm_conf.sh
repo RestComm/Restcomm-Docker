1f65cd23b1b44e27af25a717c7169bed7538aba9
lefty <eleftherios.banos@telestax.com>
2016-05-30 18:29:41 +0300

    close #75

--- scripts/restcomm_conf.sh
+++ scripts/restcomm_conf.sh
@@ -464,25 +464,26 @@ fi
 if [ -n "$RC_JAVA_OPTS_EXTRA" ]; then
     echo "Add restcomm extra java options: $RC_JAVA_OPTS_EXTRA"
 
-    JAVA_OPTS="-Xms64m -Xmx512m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true"
+    RC_JAVA_OPTS="-Xms64m -Xmx512m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true"
 
    if [[ $RC_JAVA_OPTS_EXTRA == *"Xms"* ]]; then
-        JAVA_OPTS=$(echo "$JAVA_OPTS" | sed 's/'-Xms64m'//g')
+        RC_JAVA_OPTS=$(echo "$RC_JAVA_OPTS" | sed 's/'-Xms64m'//g')
    fi
 
    if [[ $RC_JAVA_OPTS_EXTRA == *"Xmx"* ]]; then
-        JAVA_OPTS=$(echo "$JAVA_OPTS" | sed 's/'-Xmx512m'//g')
+        RC_JAVA_OPTS=$(echo "$RC_JAVA_OPTS" | sed 's/'-Xmx512m'//g')
     fi
 
     if [[ $RC_JAVA_OPTS_EXTRA == *"MaxPermSize"* ]]; then
-        JAVA_OPTS=$(echo "$JAVA_OPTS" | sed 's/'-XX:MaxPermSize=256m'//g')
+        RC_JAVA_OPTS=$(echo "$RC_JAVA_OPTS" | sed 's/'-XX:MaxPermSize=256m'//g')
     fi
 
     if [[ $RC_JAVA_OPTS_EXTRA == *"preferIPv4Stack"* ]]; then
-        JAVA_OPTS=$(echo "$JAVA_OPTS" | sed 's/'-Djava.net.preferIPv4Stack=true'//g')
+        RC_JAVA_OPTS=$(echo "$RC_JAVA_OPTS" | sed 's/'-Djava.net.preferIPv4Stack=true'//g')
     fi
 
-    sed -i 's| JAVA_OPTS="-Xms64m -Xmx512m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true"|JAVA_OPTS="$JAVA_OPTS $RC_JAVA_OPTS_EXTRA" |' $BASEDIR/bin/standalone.conf
+
+    sed -i "s| JAVA_OPTS=\"-Xms64m -Xmx512m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true\"|JAVA_OPTS=\"${RC_JAVA_OPTS} ${RC_JAVA_OPTS_EXTRA}\" |" $BASEDIR/bin/standalone.conf
 
 fi
 
