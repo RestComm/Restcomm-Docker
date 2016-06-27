27390745878ed8d8d515c9a55c6a302b3946a525
lefty <eleftherios.banos@telestax.com>
2016-06-27 10:13:05 +0300

    Fixes.

--- scripts/restcomm_toolsconf.sh
+++ scripts/restcomm_toolsconf.sh
@@ -4,6 +4,17 @@ source /etc/container_environment.sh
 
 BASEDIR=/opt/Restcomm-JBoss-AS7
 
+
+if [ -n "$GRAYLOG_SERVER" ]; then
+   echo "GRAYLOG_SERVER $GRAYLOG_SERVER"
+   sed -i "s|GRAYLOG_SERVER=.*|GRAYLOG_SERVER='${GRAYLOG_SERVER}'|" $BASEDIR/bin/restcomm/advanced.conf
+   sed -i "s|SERVERLABEL=.*|SERVERLABEL='${SERVERLABEL}'|" $BASEDIR/bin/restcomm/advanced.conf
+   sed -i "s|HD_MONITOR=.*|HD_MONITOR='${HD_MONITOR}'|" $BASEDIR/bin/restcomm/advanced.conf
+   sed -i "s|RCJVM_MONITOR=.*|RCJVM_MONITOR='${RCJVM_MONITOR}'|" $BASEDIR/bin/restcomm/advanced.conf
+   sed -i "s|RMSJVM_MONITOR=.*|RMSJVM_MONITOR='${RMSJVM_MONITOR}'|" $BASEDIR/bin/restcomm/advanced.conf
+   sed -i "s|RAM_MONITOR=.*|RAM_MONITOR='${RAM_MONITOR}'|" $BASEDIR/bin/restcomm/advanced.conf
+fi
+
 chmod +x $BASEDIR/bin/*.sh
 chmod +x $BASEDIR/bin/restcomm/*.sh
 chmod +x /opt/embed/*.sh
