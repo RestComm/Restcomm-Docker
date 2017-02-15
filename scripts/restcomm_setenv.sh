5992dcc44fc8368c9823945bbe563e20ccf5fd04
leftyb <Lefty1978!>
2017-02-15 12:21:19 +0200

    RMS5 integration fixes

--- scripts/restcomm_setenv.sh
+++ scripts/restcomm_setenv.sh
@@ -7,11 +7,12 @@ getOptions() {
 
 function jsonval {
     temp=$(getOptions $1)
+    echo "temp:$temp"
     if [ $? -eq 0 ] && [ -n "$temp" ];then
         while read -r option; do
         echo "option:$option"
         IFS=':' read ar1 ar2 <<< "$option"
-         #echo "$ar2 > /etc/container_environment/$ar1"
+         echo "$ar2 > /etc/container_environment/$ar1"
          echo -e "$ar2" | xargs > /etc/container_environment/$ar1
         done <<< "$temp"
     fi
@@ -20,5 +21,6 @@ function jsonval {
 OPTS=( "RCBCONF_" "RCADVCONF_" "EXTCONF_" "LBCONF_" "RMSCONF_" "CLI_")
 
   for opt in "${OPTS[@]}"; do
+        echo "opt:$opt"
        jsonval $opt
    done
